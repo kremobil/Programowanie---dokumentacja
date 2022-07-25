9479da1bd52a2733e888024c184e6b8cccc01f25
kremobil <kremobil@wp.pl>
2022-07-25 23:32:19 +0200

    clear app

--- Vue/kurs Hello Roman/Mój projekt/project/src/App.vue
+++ Vue/kurs Hello Roman/Mój projekt/project/src/App.vue
@@ -6,25 +6,9 @@
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" scoped>
 </style>
--- Vue/kurs Hello Roman/Mój projekt/project/src/views/HomeView.vue
+++ Vue/kurs Hello Roman/Mój projekt/project/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="search">
+      <label htmlFor="search">Wyszukaj w Nasa</label>
+      <input type="text" id="search" name="search">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style lang="sass" scoped>
+
+</style>
